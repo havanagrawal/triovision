72d8bc7b396b087badb1035a76b79b6f2d47b70d
Havan <havanagrawal@gmail.com>
2017-05-23 21:20:22 +0530

    Add failing test for rotated card matching

--- src/test/java/com/havana/games/triovision/model/CardAndBoardMatcherTest.java
+++ src/test/java/com/havana/games/triovision/model/CardAndBoardMatcherTest.java
@@ -1,8 +1,10 @@
 package com.havana.games.triovision.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CardAndBoardMatcherTest {
@@ -46,6 +48,14 @@ public class CardAndBoardMatcherTest {
 				.build();
 		
 		assertFalse(board.matches(card));
+	}
+	
+	@Test
+	@Ignore
+	public void testCardMatchesInitialConfigurationRotatedClockwiseOnce() {
+		Card card = Card.builder().topLeft(Pawn.YELLOW).middleRight(Pawn.BLUE).bottomRight(Pawn.BLUE).build();
+		
+		assertTrue(board.matches(card));
 	}
 
 }
