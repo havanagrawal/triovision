c8d4ad6d72662667940085b51e84c396b2b1e600
Havan <havanagrawal@gmail.com>
2017-05-30 22:03:39 +0530

    Add UUID field to Player

--- src/main/java/com/havana/games/triovision/model/Player.java
+++ src/main/java/com/havana/games/triovision/model/Player.java
@@ -2,22 +2,28 @@ package com.havana.games.triovision.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Player {
 
 	private List<Card> wonCards;
+	private UUID playerId;
 	
 	public Player() {
 		wonCards = new ArrayList<>();
+		playerId = UUID.randomUUID();
 	}
 	
 	public List<Card> getWonCards() {
-		// TODO Auto-generated method stub
 		return wonCards;
 	}
 
 	public void addWonCard(Card card) {
 		wonCards.add(card);
+	}
+
+	public UUID getPlayerId() {
+		return playerId;
 	}
 
 }
